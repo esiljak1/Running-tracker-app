265fd9d28d88b01da216988b127da7e3556adffc
Emin Šiljak <56275045+esiljak1@users.noreply.github.com>
2021-03-14 22:28:31 +0100

    It doesn't search for location

--- app/src/main/java/com/etf/unsa/ba/zavrsnirad_esiljak1/FirstFragment.kt
+++ app/src/main/java/com/etf/unsa/ba/zavrsnirad_esiljak1/FirstFragment.kt
@@ -1,12 +1,20 @@
 package com.etf.unsa.ba.zavrsnirad_esiljak1
 
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.ba.zavrsnirad_esiljak1.R
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -14,6 +22,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class FirstFragment : Fragment() {
 
+    private lateinit var mMap: GoogleMap
     private val callback = OnMapReadyCallback { googleMap ->
         /**
          * Manipulates the map once available.
@@ -24,22 +33,49 @@ class FirstFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        println("Ovdje")
+        mMap = googleMap
     }
+    private lateinit var locationListener: LocationListener
+    private lateinit var locationManager: LocationManager
+
+    private val MIN_TIME: Long = 1000
+    private val MIN_DIST: Float = 5f
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PackageManager.PERMISSION_GRANTED)
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        locationListener = object: LocationListener{
+            override fun onLocationChanged(location: Location) {
+                println("Listener")
+                val latLng = LatLng(location.latitude, location.longitude)
+                mMap.addMarker(MarkerOptions().position(latLng).title("Marker in Sydney"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+            }
+
+        }
+
+        locationManager = activity!!.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        println("Manager: " + locationManager)
+
+        if (ActivityCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            println("Nemam prava")
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, locationListener)
     }
 }
