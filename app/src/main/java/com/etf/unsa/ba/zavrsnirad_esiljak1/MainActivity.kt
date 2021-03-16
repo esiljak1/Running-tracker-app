dc8083eb35115e4bff68266e476f2e2db8de346d
Emin Šiljak <56275045+esiljak1@users.noreply.github.com>
2021-03-17 00:25:04 +0100

    Added changing of values on run screen

--- app/src/main/java/com/etf/unsa/ba/zavrsnirad_esiljak1/MainActivity.kt
+++ app/src/main/java/com/etf/unsa/ba/zavrsnirad_esiljak1/MainActivity.kt
@@ -1,36 +1,12 @@
 package com.etf.unsa.ba.zavrsnirad_esiljak1
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.ba.zavrsnirad_esiljak1.R
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.OnSuccessListener
 
 class MainActivity : AppCompatActivity() {
-
-    private val PERMISSION_FINE_LOCATION: Int = 99
-    private val EPS: Double = 0.001
-    private var totalDistance: Double = 0.0
-
-    private lateinit var tv_lat: TextView
-    private lateinit var tv_long: TextView
-    private lateinit var tv_distance: TextView
-
-    private lateinit var fusedLocationProvider: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
-    private lateinit var locationCallback: LocationCallback
-    private lateinit var previousLocation: Location
 
     private var startup: Fragment? = null
 
@@ -49,82 +25,10 @@ class MainActivity : AppCompatActivity() {
             fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
         }
 
-//        tv_lat = findViewById(R.id.tv_lat)
-//        tv_long = findViewById(R.id.tv_long)
-//        tv_distance = findViewById(R.id.tv_distance)
-//
-//        locationCallback = object : LocationCallback() {
-//            override fun onLocationResult(p0: LocationResult) {
-//                super.onLocationResult(p0)
-//
-//                val currentLocation = p0.lastLocation
-//
-//                if(abs(currentLocation.latitude - previousLocation.latitude) > EPS || abs(currentLocation.longitude - previousLocation.longitude) > EPS){
-//                    val distance: Float = currentLocation.distanceTo(previousLocation)
-//                    totalDistance += distance
-//                }
-//
-//                updateUIValues(currentLocation)
-//            }
-//        }
-//
-//        locationRequest = LocationRequest()
-//        locationRequest.interval = 1000 * 30
-//        locationRequest.fastestInterval = 1000 * 5
-//
-//        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
-//
-//        updateGPS()
-//        startLocationUpdates()
     }
 
-    private fun updateGPS(){
-        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            fusedLocationProvider.lastLocation.addOnSuccessListener(this, object : OnSuccessListener<Location>{
-                override fun onSuccess(p0: Location?) {
-                    previousLocation = Location(p0)
-                    updateUIValues(p0)
-                }
 
-            })
-        }else{
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_FINE_LOCATION)
-        }
-    }
 
-    private fun updateUIValues(location: Location?) {
-        if(location != null){
-            tv_lat.text = location.latitude.toString()
-            tv_long.text = location.longitude.toString()
-            tv_distance.text = totalDistance.toString()
-        }
-    }
 
-    private fun startLocationUpdates(){
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return
-        }
-
-        fusedLocationProvider.requestLocationUpdates(locationRequest, locationCallback, null)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if(requestCode == PERMISSION_FINE_LOCATION){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                updateGPS()
-            }else{
-                Toast.makeText(this, "App doesn't have permission to use location", Toast.LENGTH_SHORT).show()
-                finish()
-            }
-        }
-    }
 }
--- app/src/main/java/com/etf/unsa/ba/zavrsnirad_esiljak1/RunningFragment.kt
+++ app/src/main/java/com/etf/unsa/ba/zavrsnirad_esiljak1/RunningFragment.kt
@@ -1,58 +1,147 @@
 package com.etf.unsa.ba.zavrsnirad_esiljak1
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.ba.zavrsnirad_esiljak1.R
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnSuccessListener
+import kotlin.math.abs
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RunningFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RunningFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val PERMISSION_FINE_LOCATION: Int = 99
+    private val EPS: Double = 0.001
+    private var totalDistance: Double = 0.0
+    private var startTime: Long = 0
+
+    private lateinit var tv_time: TextView
+    private lateinit var tv_speed: TextView
+    private lateinit var tv_latitude: TextView
+    private lateinit var tv_longitude: TextView
+    private lateinit var tv_distance: TextView
+
+    private lateinit var fusedLocationProvider: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var previousLocation: Location
+    private lateinit var handler: Handler
+    private lateinit var runnable: Runnable
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_running, container, false)
+
+        tv_time = view.findViewById(R.id.tv_time)
+        tv_speed = view.findViewById(R.id.tv_speed)
+        tv_latitude = view.findViewById(R.id.tv_latitude)
+        tv_longitude = view.findViewById(R.id.tv_longitude)
+        tv_distance = view.findViewById(R.id.tv_distance)
+
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                val currentLocation = p0.lastLocation
+                if(abs(currentLocation.latitude - previousLocation.latitude) > EPS || abs(currentLocation.longitude - previousLocation.longitude) > EPS){
+                    val distance: Float = currentLocation.distanceTo(previousLocation)
+                    totalDistance += distance
+                }
+
+                updateUIValues(currentLocation)
+            }
+        }
+
+        locationRequest = LocationRequest()
+        locationRequest.interval = 1000 * 30
+        locationRequest.fastestInterval = 1000 * 5
+
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        updateGPS()
+        startLocationUpdates()
+
+//        timer.scheduleAtFixedRate(object : TimerTask(){
+//            override fun run() {
+//                elapsedTimeSeconds++
+//                tv_time.text = elapsedTimeSeconds.toString()
+//            }
+//
+//        }, 1000, 1000)
+
+        startTime = System.currentTimeMillis()
+
+        handler = Handler()
+        runnable = object : Runnable{
+            override fun run() {
+                val millis: Long = System.currentTimeMillis() - startTime
+                val seconds: Int = ((millis / 1000) % 60).toInt()
+
+                tv_time.text = seconds.toString()
+                handler.postDelayed(this, 500)
+            }
+        }
+
+        handler.postDelayed(runnable, 0)
+
+        return view
+    }
+
+    private fun updateGPS() {
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(activity)
+
+        if(ActivityCompat.checkSelfPermission(activity!!, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProvider.lastLocation.addOnSuccessListener(activity!!, object : OnSuccessListener<Location> {
+                override fun onSuccess(p0: Location?) {
+                    previousLocation = Location(p0)
+                    updateUIValues(p0!!)
+                }
+
+            })
+        }else{
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_FINE_LOCATION)
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_running, container, false)
+    private fun updateUIValues(location: Location) {
+        tv_latitude.text = String.format("%.2f", location.latitude)
+        tv_longitude.text = String.format("%.2f", location.longitude)
+        tv_distance.text = String.format("%.2f", totalDistance)
+        tv_speed.text = String.format("%.2f", location.speed)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RunningFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                RunningFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private fun startLocationUpdates(){
+
+        if (ActivityCompat.checkSelfPermission(activity!!, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity!!, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        fusedLocationProvider.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == PERMISSION_FINE_LOCATION){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                updateGPS()
+            }else{
+                Toast.makeText(activity, "App doesn't have permission to use location", Toast.LENGTH_SHORT).show()
+                activity!!.finish()
+            }
+        }
     }
 }
